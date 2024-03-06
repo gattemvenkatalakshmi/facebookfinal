5a5d171c55bde6bc97e50fd6d5e3082bd7df3315
gattemvenkatalakshmi <73790386+gattemvenkatalakshmi@users.noreply.github.com>
2024-03-06 12:17:56 +0530

    Update settings.py

--- FacebookFinal/settings.py
+++ FacebookFinal/settings.py
@@ -77,12 +77,9 @@ WSGI_APPLICATION = 'FacebookFinal.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'facebook',
-        'USER': 'postgres',
-        'PASSWORD': 'postgres',
-        'HOST':'localhost'
+   "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
     }
 }
 
